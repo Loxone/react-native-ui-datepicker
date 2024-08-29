a800cbe6de1aacad9b4e0e10a2ef6f069e21dff0
Max Schwarz <maximilian.schwarz@loxone.com>
2024-08-29 17:16:05 +0200

    feat: added disabling of months and years that would be out of limits

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Text, View, Pressable, StyleSheet } from 'react-native';
 import { useCalendarContext } from '../CalendarContext';
-import { getParsedDate, getMonths } from '../utils';
+import { getParsedDate, getMonths, getDateYear } from '../utils';
 
 const MonthSelector = () => {
-  const { currentDate, onSelectMonth, theme, formatters } = useCalendarContext();
+  const { currentDate, onSelectMonth, theme, formatters, maxDate, minDate } = useCalendarContext();
   const { month } = getParsedDate(currentDate);
+  const parsedMinDate = minDate ? getParsedDate(minDate) : undefined;
+  const parsedMaxDate = maxDate ? getParsedDate(maxDate) : undefined;
+  const currentYear = getDateYear(currentDate);
+  const minMonthToUse = parsedMinDate && currentYear === parsedMinDate.year ? parsedMinDate.month : undefined;
+  const maxMonthToUse = parsedMaxDate && currentYear === parsedMaxDate.year ? parsedMaxDate.month : undefined;
 
   return (
     <View style={styles.container} testID="month-selector">
@@ -24,15 +29,23 @@ const MonthSelector = () => {
               ? { color: '#fff', ...theme?.selectedTextStyle }
               : theme?.calendarTextStyle;
 
+          let isOutOfRange = false;
+          if (minMonthToUse && index < minMonthToUse) {
+            isOutOfRange = true;
+          } else if (maxMonthToUse && index > maxMonthToUse) {
+            isOutOfRange = true;
+          }
+
           const formattedItem = formatters?.monthName ? formatters.monthName(new Date(2024, index, 1)) : item;
 
           return (
             <Pressable
               key={index}
-              style={styles.monthCell}
+              style={[styles.monthCell, isOutOfRange ? styles.disabledMonthCell : null]}
               onPress={() => onSelectMonth(index)}
               accessibilityRole="button"
               accessibilityLabel={item}
+              disabled={isOutOfRange}
             >
               <View
                 style={[
@@ -65,6 +78,9 @@ const styles = StyleSheet.create({
   },
   monthCell: {
     width: '33.3%',
+  },
+  disabledMonthCell: {
+    opacity: 0.3,
   },
   month: {
     paddingVertical: 15,
--- src/components/YearSelector.tsx
+++ src/components/YearSelector.tsx
@@ -11,12 +11,14 @@ import { useCalendarContext } from '../CalendarContext';
 import { getDateYear, getYearRange } from '../utils';
 
 const YearSelector = () => {
-  const { currentDate, currentYear, date, onSelectYear, theme } =
+  const { currentDate, currentYear, date, onSelectYear, theme, minDate, maxDate } =
     useCalendarContext();
+  const minYear = minDate ? getDateYear(minDate) : undefined;
+  const maxYear = maxDate ? getDateYear(maxDate) : undefined;
   const selectedYear = getDateYear(date);
 
   const generateCells = useCallback(() => {
-    const years = getYearRange(currentYear);
+    const years = getYearRange(currentYear, minYear, maxYear);
     const activeYear = getDateYear(currentDate);
     const column = years.map((year) => {
       const activeItemStyle: ViewStyle =
@@ -41,13 +43,16 @@ const YearSelector = () => {
             }
           : { ...theme?.calendarTextStyle };
 
+      const isDisabled = year > maxYear || year < minYear;
+
       return (
         <Pressable
           key={year}
           onPress={() => onSelectYear(year)}
-          style={styles.yearCell}
+          style={[styles.yearCell, isDisabled ? styles.disabledYearCell : {}]}
           accessibilityRole="button"
           accessibilityLabel={year.toString()}
+          disabled={isDisabled}
         >
           <View
             style={[styles.year, theme?.yearContainerStyle, activeItemStyle]}
@@ -60,7 +65,7 @@ const YearSelector = () => {
       );
     });
     return column;
-  }, [onSelectYear, selectedYear, currentYear, currentDate, theme]);
+  }, [onSelectYear, selectedYear, currentYear, currentDate, theme, minYear, maxYear]);
 
   return (
     <View style={styles.container} testID="year-selector">
@@ -79,6 +84,9 @@ const styles = StyleSheet.create({
   },
   yearCell: {
     width: '33.3%',
+  },
+  disabledYearCell: {
+    opacity: 0.3,
   },
   years: {
     flexDirection: 'row',
