766f93314c6ad1fd0ec906a2788bc5c8c2922c86
bialopa <patryk.bialon@loxone.com>
2025-05-07 10:13:02 +0200

    fix: tie day grid generation to timezone

--- src/utils.ts
+++ src/utils.ts
@@ -506,7 +506,7 @@ export const getMonthDays = (
   numerals: Numerals,
   timeZone: string | undefined
 ): CalendarDay[] => {
-  const date = dayjs(datetime).tz(timeZone);
+  const date = dayjs.tz(datetime, timeZone);
 
   const prevDays = showOutsideDays
     ? Array.from({ length: prevMonthOffset }, (_, index) => {
